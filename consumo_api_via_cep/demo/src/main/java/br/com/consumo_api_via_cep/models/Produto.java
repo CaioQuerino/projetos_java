9d2d2a422aca7bccd99d4d9915b803be4c8cca8a
Caio Querino <caioquerino04@gmail.com>
2025-09-14 15:01:16 -0300

    feat: nova classe produto

---
+++ consumo_api_via_cep/demo/src/main/java/br/com/consumo_api_via_cep/models/Produto.java
@@ -0,0 +1,70 @@
+package br.com.consumo_api_via_cep.models;
+
+public class Produto {
+    private int id;
+    private String nome;
+    private double preco;
+    private String descricao;
+    private String categoria;
+    private int quantidade;
+
+    Produto() {}
+
+    Produto(int id, String nome, double preco, String descricao, 
+                                String categoria, int quantidade) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.descricao = descricao;
+        this.categoria = categoria;
+        this.quantidade = quantidade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}
