284b7194b489d383e8e48adecb08f0b7854b0855
Caio Querino <caioquerino04@gmail.com>
2025-09-14 00:20:50 -0300

    feat: criando a classe pessoa

---
+++ consumo_api_via_cep/demo/src/main/java/br/com/caioq/models/Pessoa.java
@@ -0,0 +1,78 @@
+package br.com.caioq.models;
+
+import br.com.caioq.services.ServicoEndereco;
+
+public class Pessoa {
+    private String nome;
+    private String email;
+    private String endereco;
+    private int idade;
+
+    public Pessoa() {}
+
+    public Pessoa(String nome, String email, String endereco, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.endereco = endereco;
+        this.idade = idade;
+    }
+
+    public Pessoa(String nome, String email, String cep, int idade, boolean usarCep) {
+        this.nome = nome;
+        this.email = email;
+        if (usarCep) {
+            this.endereco = ServicoEndereco.buscarEnderecoPorCep(cep);
+        } else {
+            this.endereco = cep; 
+        }
+        this.idade = idade;
+    }
+
+    // Getters
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    // Setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public void setEnderecoPorCep(String cep) {
+        this.endereco = ServicoEndereco.buscarEnderecoPorCep(cep);
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", endereco='" + endereco + '\'' +
+                ", idade=" + idade +
+                '}';
+    }
+}
