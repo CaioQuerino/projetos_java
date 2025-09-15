dcf80a955a498b7828b2aed78572bc84d5739b58
Caio Querino <caioquerino04@gmail.com>
2025-09-15 20:21:17 -0300

    feat: adicionando método registrarPessoa() no ServicoRegisto

---
+++ consumo_api_via_cep/demo/src/main/java/br/com/consumo_api_via_cep/services/ServicoRegistro.java
@@ -0,0 +1,27 @@
+package br.com.consumo_api_via_cep.services;
+
+import br.com.consumo_api_via_cep.data.PessoaData;
+import br.com.consumo_api_via_cep.models.Pessoa;
+
+public class ServicoRegistro {
+    
+    private PessoaData pessoaData;
+    
+    public ServicoRegistro() {
+        this.pessoaData = new PessoaData();
+    }
+    
+    public ServicoRegistro(PessoaData pessoaData) {
+        this.pessoaData = pessoaData;
+    }
+    
+    public String registrarPessoa(String nome, String email, String endereco, int idade) {
+        Pessoa pessoa = ServicoPessoa.criarPessoaValidada(nome, email, endereco, idade);
+        
+        if (pessoa != null) {
+            return pessoaData.salvar(pessoa);
+        } else {
+            return "Erro: Não foi possível criar a pessoa. Verifique os dados.";
+        }
+    }
+} 
