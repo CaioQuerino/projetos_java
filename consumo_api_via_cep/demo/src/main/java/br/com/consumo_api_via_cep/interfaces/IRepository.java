8b5d93cc3f37bdfe8cf20c79fc2f55e914afa957
Caio Querino <caioquerino04@gmail.com>
2025-09-15 22:09:05 -0300

    refactor: os métodos da interface ñ são mais abstratos

--- consumo_api_via_cep/demo/src/main/java/br/com/consumo_api_via_cep/interfaces/IRepository.java
+++ consumo_api_via_cep/demo/src/main/java/br/com/consumo_api_via_cep/interfaces/IRepository.java
@@ -1,9 +1,9 @@
 package br.com.consumo_api_via_cep.interfaces;
 
 public interface IRepository {
-    abstract String salvar(Object obj);
-    abstract String atualizar(int id, Object obj);
-    abstract String deletar(int id);
-    abstract Object buscarPorId(int id);
-    abstract Object[] buscarTodos();
+    String salvar(Object obj);
+    String atualizar(int id, Object obj);
+    String deletar(int id);
+    Object buscarPorId(int id);
+    Object[] buscarTodos();
 }
