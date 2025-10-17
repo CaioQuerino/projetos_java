da79e026d225d3bb983acb2ee9f316952fd7d561
Caio Querino <caioquerino04@gmail.com>
2025-10-17 14:53:27 -0300

    feat: adicionando o controller sa Conta

---
+++ conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/controllers/ContaController.java
@@ -0,0 +1,33 @@
+package br.com.conta_bancaria.conta_bancaria.controllers;
+
+import org.springframework.web.bind.annotation.*;
+import br.com.conta_bancaria.conta_bancaria.models.*;
+import br.com.conta_bancaria.conta_bancaria.services.ContaService;
+
+@RestController
+@RequestMapping("/contas")
+public class ContaController {
+
+    private final ContaService contaService;
+
+    public ContaController(ContaService contaService) {
+        this.contaService = contaService;
+    }
+
+    @PostMapping
+    public Conta criarConta(@RequestBody Conta conta) {
+        return this.contaService.criarConta(
+            conta.getNumeroConta(),
+            conta.getTipoConta(),
+            conta.getSaldo(),
+            conta.getCliente(),
+            conta.getBanco(),
+            conta.getSenha()
+        );
+    }
+
+    @GetMapping("/{numeroConta}")
+    public Conta buscarConta(@PathVariable String numeroConta) {
+        return contaService.buscarPorNumero(numeroConta);
+    }
+}
