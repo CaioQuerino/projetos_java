ba739b8bde3756645038d528712df3a0102432b2
Caio Querino <caioquerino04@gmail.com>
2025-10-17 14:54:12 -0300

    feat: ContaBancariaApplication (Roda a aplicação)

---
+++ conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/ContaBancariaApplication.java
@@ -0,0 +1,13 @@
+package br.com.conta_bancaria.conta_bancaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaBancariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaBancariaApplication.class, args);
+	}
+
+}
