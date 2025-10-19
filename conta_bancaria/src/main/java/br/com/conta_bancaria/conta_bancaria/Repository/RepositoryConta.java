0148edffdb98e6a1aea5f24a75a4816542761f9e
Caio Querino <caioquerino04@gmail.com>
2025-10-19 16:14:49 -0300

    feat: adicionando o repositório da conta

---
+++ conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/Repository/RepositoryConta.java
@@ -0,0 +1,12 @@
+package br.com.conta_bancaria.conta_bancaria.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+import br.com.conta_bancaria.conta_bancaria.models.Conta;
+
+@Repository
+public interface RepositoryConta extends JpaRepository<Conta, Long> {
+    Optional<Conta> findByNumeroConta(String numeroConta);
+}
