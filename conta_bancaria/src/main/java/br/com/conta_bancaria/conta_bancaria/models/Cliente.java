d6839dd8024ff66e186470e69759f7f7c136016a
Caio Querino <caioquerino04@gmail.com>
2025-10-19 16:06:13 -0300

    feat: adicionando o model Cliente

---
+++ conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/models/Cliente.java
@@ -0,0 +1,64 @@
+package br.com.conta_bancaria.conta_bancaria.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String nome;
+    
+    @Embedded
+    private ViaCep endereco;
+    
+    private String telefone;
+    private String cpf;
+    private String agencia;
+    private String codigoBanco;
+
+    public Cliente() {}
+
+    public Cliente(String nome, ViaCep endereco, String telefone, String cpf, 
+                                        String agencia, String codigoBanco) {
+
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.cpf = cpf;
+        this.agencia = agencia;
+        this.codigoBanco = codigoBanco;
+    }
+
+    public Long getId() { return id; }
+    
+    public void setId(Long id) { this.id = id; }
+    
+    public String getNome() { return nome; }
+    
+    public void setNome(String nome) { this.nome = nome; }
+    
+    public ViaCep getEndereco() { return endereco; }
+    
+    public void setEndereco(ViaCep endereco) { this.endereco = endereco; }
+    
+    public String getTelefone() { return telefone; }
+    
+    public void setTelefone(String telefone) { this.telefone = telefone; }
+    
+    public String getCpf() { return cpf; }
+    
+    public void setCpf(String cpf) { this.cpf = cpf; }
+    
+    public String getAgencia() { return agencia; }
+    
+    public void setAgencia(String agencia) { this.agencia = agencia; }
+    
+    public String getCodigoBanco() { return codigoBanco; }
+    
+    public void setCodigoBanco(String codigoBanco) { this.codigoBanco = codigoBanco; }
+    
+}
