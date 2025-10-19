6a2ba864bf672f6d6a52e5e7e8abbc3e6b59639e
Caio Querino <caioquerino04@gmail.com>
2025-10-19 18:17:40 -0300

    feat: adicionando findAllByOrderByIdAsc

--- conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/Repository/RepositoryConta.java
+++ conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/Repository/RepositoryConta.java
@@ -5,8 +5,12 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 import br.com.conta_bancaria.conta_bancaria.models.Conta;
+import java.util.List;
 
 @Repository
 public interface RepositoryConta extends JpaRepository<Conta, Long> {
     Optional<Conta> findByNumeroConta(String numeroConta);
+    
+    List<Conta> findAllByOrderByIdAsc();
+
 }
