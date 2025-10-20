805314ff5610afdf72e49b6bafa9a35902c8beb7
Caio Querino <caioquerino04@gmail.com>
2025-10-20 09:06:16 -0300

    fix: removendo as rotas clientes e bancos

--- conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/controllers/ContaController.java
+++ conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/controllers/ContaController.java
@@ -2,8 +2,6 @@ package br.com.conta_bancaria.conta_bancaria.controllers;
 
 import org.springframework.web.bind.annotation.*;
 import br.com.conta_bancaria.conta_bancaria.models.Conta;
-import br.com.conta_bancaria.conta_bancaria.models.Banco;
-import br.com.conta_bancaria.conta_bancaria.models.Cliente;
 import br.com.conta_bancaria.conta_bancaria.services.ContaService;
 
 import java.util.List;
@@ -38,15 +36,5 @@ public class ContaController {
     @GetMapping
     public List<Conta> listarTodasContas() {
         return contaService.listarTodas();
-    }
-
-    @GetMapping("/bancos")
-    public List<Banco> listarTodosBancos() {
-        return contaService.listarTodosBancos();
-    }
-
-    @GetMapping("/clientes")
-    public List<Cliente> listarTodosClientes() {
-        return contaService.listarTodosClientes();
     }
 }
