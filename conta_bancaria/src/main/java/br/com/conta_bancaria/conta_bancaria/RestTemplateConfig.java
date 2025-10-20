54b4de8ffdaa0695fb13a84f7796934957bb1c9e
Caio Querino <caioquerino04@gmail.com>
2025-10-19 21:45:37 -0300

    feat: adicionando cors

--- conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/RestTemplateConfig.java
+++ conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/RestTemplateConfig.java
@@ -3,6 +3,8 @@ package br.com.conta_bancaria.conta_bancaria;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class RestTemplateConfig {
@@ -10,5 +12,19 @@ public class RestTemplateConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500", "file://")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
+            }
+        };
     }
 }
