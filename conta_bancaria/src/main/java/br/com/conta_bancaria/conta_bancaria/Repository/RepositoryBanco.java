88e7b7a3d9e4499a6643e85c6866f1bb301d87eb
Caio Querino <caioquerino04@gmail.com>
2025-10-22 00:52:53 -0300

    feat: adicionando outros métodos ao RepositoryBanco

--- conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/Repository/RepositoryBanco.java
+++ conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/Repository/RepositoryBanco.java
@@ -1,10 +1,17 @@
 package br.com.conta_bancaria.conta_bancaria.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import br.com.conta_bancaria.conta_bancaria.models.Banco;
 
 @Repository
 public interface RepositoryBanco extends JpaRepository<Banco, Long> {
-    Banco findByCnpj(String cnpj);
+    Optional<Banco> findByCnpj(String cnpj);
+    Optional<Banco> findByNome(String nome);
+    Optional<Banco> findByCodigoBanco(String codigoBanco);
+    List<Banco> findAllByCodigoBanco(String codigoBanco);
+    boolean existsByCodigoBanco(String codigoBanco);
 }
