fa55ef02089a21d6fb212d994fd9367247be2787
Caio Querino <caioquerino04@gmail.com>
2025-10-26 15:40:24 -0300

    feat: adicionando model do Task

---
+++ task/src/main/java/br/com/task/task/models/Task.java
@@ -0,0 +1,42 @@
+package br.com.task.task.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tasks")
+
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String tarefa;
+    private String descricao;
+    private boolean concluido = false;
+
+    public Task() {}
+
+    public Task(Long id, String tarefa, String descricao, boolean concluido) 
+    {
+        this.id = id;
+        this.tarefa = tarefa;
+        this.descricao = descricao;
+        this.concluido = concluido;
+    }
+
+    public Long getId() { return this.id; }
+
+    public void setId(Long id) { this.id = id; }
+
+    public String getTarefa() { return this.tarefa; }
+
+    public void setTarefa(String tarefa) { this.tarefa = tarefa; }
+
+    public String getDescricao() { return this.descricao; }
+
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+
+    public boolean getConcluido() { return this.concluido; }
+
+    public void setConcluido(boolean concluido) { this.concluido = concluido; }
+}
