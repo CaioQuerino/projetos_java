b0341b523c6c37a46a0a747aad582388e7165844
Caio Querino <caioquerino04@gmail.com>
2025-10-28 03:46:24 -0300

    feat: adicionando dto chamado CreateBancoRequest

---
+++ conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/dto/requests/banco/CreateBancoRequest.java
@@ -0,0 +1,49 @@
+package br.com.conta_bancaria.conta_bancaria.dto.requests.banco;
+
+import br.com.conta_bancaria.conta_bancaria.models.ViaCep;
+
+public class CreateBancoRequest {
+    private String nome;
+    private ViaCep endereco;
+    private String telefone;
+    private String cnpj;
+    private String agencia;
+    private String codigoBanco;
+
+    public CreateBancoRequest() {}
+
+    public CreateBancoRequest(String nome, ViaCep endereco, String telefone, 
+                             String cnpj, String agencia, String codigoBanco) 
+    {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.cnpj = cnpj;
+        this.agencia = agencia;
+        this.codigoBanco = codigoBanco;
+    }
+
+    public String getNome() { return nome; }
+
+    public void setNome(String nome) { this.nome = nome; }
+
+    public ViaCep getEndereco() { return endereco; }
+
+    public void setEndereco(ViaCep endereco) { this.endereco = endereco; }
+
+    public String getTelefone() { return telefone; }
+
+    public void setTelefone(String telefone) { this.telefone = telefone; }
+
+    public String getCnpj() { return cnpj; }
+
+    public void setCnpj(String cnpj) { this.cnpj = cnpj; }
+
+    public String getAgencia() { return agencia; }
+
+    public void setAgencia(String agencia) { this.agencia = agencia; }
+
+    public String getCodigoBanco() { return codigoBanco; }
+
+    public void setCodigoBanco(String codigoBanco) { this.codigoBanco = codigoBanco; }
+}
