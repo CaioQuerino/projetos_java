0756aa57fd4e7493e5eb8cbf3f71008b533648d8
Caio Querino <caioquerino04@gmail.com>
2025-11-01 18:17:57 -0300

    🔄 refactor: aplica Lombok (@Getter, @Setter, @AllArgsConstructor) no UpdateClienteRequest
    - 🧹 Remove construtor e métodos de acesso manuais
    - 📦 Usa Lombok para simplificar estrutura do DTO de atualização
    - ✅ Mantém todos os campos necessários para edição de dados do cliente

--- conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/dto/requests/cliente/UpdateClienteRequest.java
+++ conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/dto/requests/cliente/UpdateClienteRequest.java
@@ -2,6 +2,13 @@ package br.com.conta_bancaria.conta_bancaria.dto.requests.cliente;
 
 import br.com.conta_bancaria.conta_bancaria.models.ViaCep;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class UpdateClienteRequest {
     private String nome;
     private ViaCep endereco;
@@ -9,30 +16,4 @@ public class UpdateClienteRequest {
     private String cpf;
     private String agencia;
     private String codigoBanco;
-
-    public UpdateClienteRequest() {}
-
-    public String getNome() { return nome; }
-
-    public void setNome(String nome) { this.nome = nome; }
-
-    public ViaCep getEndereco() { return endereco; }
-
-    public void setEndereco(ViaCep endereco) { this.endereco = endereco; }
-
-    public String getTelefone() { return telefone; }
-    
-    public void setTelefone(String telefone) { this.telefone = telefone; }
-
-    public String getCpf() { return cpf; }
-
-    public void setCpf(String cpf) { this.cpf = cpf; }
-
-    public String getAgencia() { return agencia; }
-
-    public void setAgencia(String agencia) { this.agencia = agencia; }
-
-    public String getCodigoBanco() { return codigoBanco; }
-    
-    public void setCodigoBanco(String codigoBanco) { this.codigoBanco = codigoBanco; }
 }
