b0ea2c628c3e79e8c5d7b9e18b28ec4daa9b7dcd
Caio Querino <caioquerino04@gmail.com>
2025-11-01 12:54:38 -0300

    🔄 refactor: aplica Lombok (@Getter, @Setter) na classe ViaCep
    - 🧹 Remove métodos manuais de acesso aos atributos
    - 🧾 Mantém construtor completo para inicialização de endereço
    - 📦 Reduz boilerplate e melhora legibilidade do modelo embutido

--- conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/models/ViaCep.java
+++ conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/models/ViaCep.java
@@ -3,6 +3,11 @@ package br.com.conta_bancaria.conta_bancaria.models;
 import br.com.conta_bancaria.conta_bancaria.interfaces.ViaCepDto;
 import jakarta.persistence.Embeddable;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @Embeddable
 public class ViaCep implements ViaCepDto {
     private String cep;
@@ -23,28 +28,4 @@ public class ViaCep implements ViaCepDto {
         this.localidade = localidade;
         this.uf = uf;
     }
-
-    public String getCep() { return cep; }
-    
-    public void setCep(String cep) { this.cep = cep; }
-    
-    public String getLogradouro() { return logradouro; }
-    
-    public void setLogradouro(String logradouro) { this.logradouro = logradouro; }
-    
-    public String getComplemento() { return complemento; }
-    
-    public void setComplemento(String complemento) { this.complemento = complemento; }
-    
-    public String getBairro() { return bairro; }
-    
-    public void setBairro(String bairro) { this.bairro = bairro; }
-    
-    public String getLocalidade() { return localidade; }
-    
-    public void setLocalidade(String localidade) { this.localidade = localidade; }
-    
-    public String getUf() { return uf; }
-    
-    public void setUf(String uf) { this.uf = uf; }
 }
