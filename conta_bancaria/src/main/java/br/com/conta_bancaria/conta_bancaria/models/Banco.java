1be585085c3ef1e909b253e5dc78e10d3cf86307
Caio Querino <caioquerino04@gmail.com>
2025-11-01 12:50:19 -0300

    🔄 refactor: aplica Lombok (@Getter, @Setter) na entidade Banco
    - 🧹 Remove métodos manuais de getter e setter
    - 🧾 Mantém construtor personalizado para inicialização de campos
    - 📦 Reduz boilerplate e melhora legibilidade da classe

--- conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/models/Banco.java
+++ conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/models/Banco.java
@@ -2,9 +2,14 @@ package br.com.conta_bancaria.conta_bancaria.models;
 
 import jakarta.persistence.*;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
 @Table(name = "banco")
 
+@Getter
+@Setter
 public class Banco {
     
     @Id
@@ -33,32 +38,4 @@ public class Banco {
         this.agencia = agencia;
         this.codigoBanco = codigoBanco;
     }
-
-    public Long getId() { return id; }
-	
-    public void setId(Long id) { this.id = id; }
-	
-    public String getNome() { return nome; }
-	
-    public void setNome(String nome) { this.nome = nome; }
-	
-    public ViaCep getEndereco() { return endereco; }
-	
-    public void setEndereco(ViaCep endereco) { this.endereco = endereco; }
-	
-    public String getTelefone() { return telefone; }
-	
-    public void setTelefone(String telefone) { this.telefone = telefone; }
-	
-    public String getCnpj() { return cnpj; }
-	
-    public void setCnpj(String cnpj) { this.cnpj = cnpj; }
-	
-    public String getAgencia() { return agencia; }
-	
-    public void setAgencia(String agencia) { this.agencia = agencia; }
-	
-    public String getCodigoBanco() { return codigoBanco; }
-    
-    public void setCodigoBanco(String codigoBanco) { this.codigoBanco = codigoBanco; }
 }
