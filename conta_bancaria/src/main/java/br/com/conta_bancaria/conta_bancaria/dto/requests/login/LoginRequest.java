f561746b8070ae1122b816c6f263e06560008389
Caio Querino <caioquerino04@gmail.com>
2025-11-01 18:19:53 -0300

    git commit -m "🔄 refactor: aplica Lombok (@Getter, @Setter, @AllArgsConstructor) no LoginRequest
    - 🧹 Remove construtor e métodos de acesso manuais
    - 📦 Usa Lombok para simplificar estrutura do DTO de autenticação
    - ✅ Mantém campos essenciais para login via número da conta e senha

--- conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/dto/requests/login/LoginRequest.java
+++ conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/dto/requests/login/LoginRequest.java
@@ -1,21 +1,13 @@
 package br.com.conta_bancaria.conta_bancaria.dto.requests.login;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class LoginRequest {
     private String numeroConta;
     private String senha;
-
-    public LoginRequest() {}
-
-    public LoginRequest(String numeroConta, String senha) {
-        this.numeroConta = numeroConta;
-        this.senha = senha;
-    }
-
-    public String getNumeroConta() { return numeroConta; }
-
-    public void setNumeroConta(String numeroConta) { this.numeroConta = numeroConta; }
-
-    public String getSenha() { return senha; }
-    
-    public void setSenha(String senha) { this.senha = senha; }
 }
