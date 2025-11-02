45ae8911865bcf2f5943053bef490a03f915125e
Caio Querino <caioquerino04@gmail.com>
2025-11-02 17:50:50 -0300

    🔄 refactor: altera relacionamento de 'endereco' em Banco para @ManyToOne
    - 🧱 Substitui @Embedded por @ManyToOne com @JoinColumn(name = 'endereco_id')
    - 🔗 Permite reutilização de ViaCep como entidade compartilhada
    - ✅ Mantém construtor e estrutura da entidade Banco

--- conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/models/Banco.java
+++ conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/models/Banco.java
@@ -18,7 +18,8 @@ public class Banco {
     
     private String nome;
     
-    @Embedded
+    @ManyToOne
+    @JoinColumn(name = "endereco_id")
     private ViaCep endereco;
     
     private String telefone;
