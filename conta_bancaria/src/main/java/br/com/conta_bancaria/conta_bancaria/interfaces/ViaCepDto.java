4f04f197fa2da107c2bed5db0ec5b4c4dd62d624
Caio Querino <caioquerino04@gmail.com>
2025-11-04 14:27:43 -0300

    🔄 refactor: remove campo 'complemento' da interface ViaCepDto.java
    - ✂️ Elimina atributo 'complemento' da interface
    - 🔄 Alinha contrato com a nova estrutura da entidade e DTO ViaCep
    - ✅ Garante consistência entre camadas e evita redundância

--- conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/interfaces/ViaCepDto.java
+++ conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/interfaces/ViaCepDto.java
@@ -3,7 +3,6 @@ package br.com.conta_bancaria.conta_bancaria.interfaces;
 public interface ViaCepDto {
     String cep = null;
     String logradouro = null;
-    String complemento = null;
     String bairro = null;
     String localidade = null;
     String uf = null;
