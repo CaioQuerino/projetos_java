5bb28b68758d2ab87fedaa58ee6f424992dd82dd
Caio Querino <caioquerino04@gmail.com>
2025-11-07 03:57:46 -0300

    🔄 refactor: implementar interface ContaRequest em CreateContaRequest
    - CreateContaRequest agora implementa a interface ContaRequest.
    - Garante padronização com UpdateContaRequest.
    - Permite o uso genérico do ContaFactory com menor duplicação de código.

--- conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/dto/requests/conta/CreateContaRequest.java
+++ conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/dto/requests/conta/CreateContaRequest.java
@@ -1,11 +1,12 @@
 package br.com.conta_bancaria.conta_bancaria.dto.requests.conta;
 
 import lombok.*;
+import br.com.conta_bancaria.conta_bancaria.interfaces.ContaRequest;
 
 @Getter
 @Setter
 @AllArgsConstructor
-public class CreateContaRequest {
+public class CreateContaRequest implements ContaRequest {
     private String tipoConta;
     private double saldo;
     private Long clienteId;
