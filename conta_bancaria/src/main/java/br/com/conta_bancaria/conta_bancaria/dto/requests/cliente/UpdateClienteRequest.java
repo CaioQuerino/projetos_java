dab5269516c9cb92fcf63172416cecb6d62c6bbd
Caio Querino <caioquerino04@gmail.com>
2025-11-07 04:00:52 -0300

    🔄 refactor: implementar interface ClienteRequest em UpdateClienteRequest

--- conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/dto/requests/cliente/UpdateClienteRequest.java
+++ conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/dto/requests/cliente/UpdateClienteRequest.java
@@ -1,13 +1,14 @@
 package br.com.conta_bancaria.conta_bancaria.dto.requests.cliente;
 
 import br.com.conta_bancaria.conta_bancaria.models.ViaCep;
+import br.com.conta_bancaria.conta_bancaria.interfaces.ClienteRequest;
 
 import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
-public class UpdateClienteRequest {
+public class UpdateClienteRequest implements ClienteRequest {
     private String nome;
     private ViaCep endereco;
     private String telefone;
