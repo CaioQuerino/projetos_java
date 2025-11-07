ce1d2325c1b2cef1332e6b8e2bce3642c777892f
Caio Querino <caioquerino04@gmail.com>
2025-11-07 03:34:21 -0300

    🔄 refactor: unifica criação de instâncias em ViaCepFactory com interface ViaCepRequest
    - Remove duplicação de métodos fromCreate e fromUpdate
    - Implementa método genérico fromRequest com suporte a ViaCepRequest
    - Simplifica conversão de DTOs para entidades ViaCep

--- conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/factorys/viacep/ViaCepFactory.java
+++ conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/factorys/viacep/ViaCepFactory.java
@@ -1,30 +1,24 @@
 package br.com.conta_bancaria.conta_bancaria.factorys.viacep;
 
-import br.com.conta_bancaria.conta_bancaria.dto.responses.viacep.ViaCepResponse;
+import br.com.conta_bancaria.conta_bancaria.interfaces.ViaCepRequest;
 import br.com.conta_bancaria.conta_bancaria.models.ViaCep;
 
 public class ViaCepFactory {
-    public static ViaCep fromCreate(ViaCepResponse request) 
+
+    private static ViaCep from(ViaCepRequest request) 
     {
-        ViaCep viaCep = ViaCep.builder()
+        return ViaCep.builder()
             .cep(request.getCep())
             .logradouro(request.getLogradouro())
             .bairro(request.getBairro())
             .localidade(request.getLocalidade())
             .uf(request.getUf())
             .build();
-            return viaCep;
     }
 
-    public static ViaCep fromUpdate(ViaCepResponse request)
-    {
-        ViaCep vs = ViaCep.builder()
-            .cep(request.getCep())
-            .logradouro(request.getLogradouro())
-            .bairro(request.getBairro())
-            .localidade(request.getLocalidade())
-            .uf(request.getUf())
-            .build();
-            return vs;
-    }
+    public static ViaCep fromRequest(Object request) {
+        return request instanceof ViaCepRequest
+            ? from((ViaCepRequest) request)
+            : null;
+    }  
 }
