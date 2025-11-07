ba8b6f7d374abd47028f98f896b5a1e2c475a2d7
Caio Querino <caioquerino04@gmail.com>
2025-11-07 04:02:44 -0300

    deletado

--- conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/interfaces/ViaCepDto.java
+++
@@ -1,9 +0,0 @@
-package br.com.conta_bancaria.conta_bancaria.interfaces;
-
-public interface ViaCepDto {
-    String cep = null;
-    String logradouro = null;
-    String bairro = null;
-    String localidade = null;
-    String uf = null;
-}
