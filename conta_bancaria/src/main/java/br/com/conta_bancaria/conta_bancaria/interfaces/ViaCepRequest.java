abfffec2582dd8f8bfbea704c3ca229ea808290a
Caio Querino <caioquerino04@gmail.com>
2025-11-07 03:43:53 -0300

    ✨ feat: adicionar interface ViaCepRequest para padronizar requisições de endereço via CEP

---
+++ conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/interfaces/ViaCepRequest.java
@@ -0,0 +1,9 @@
+package br.com.conta_bancaria.conta_bancaria.interfaces;
+
+public interface ViaCepRequest {
+    String getCep();
+    String getLogradouro();
+    String getBairro();
+    String getLocalidade();
+    String getUf();    
+}
