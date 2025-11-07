e98c9737c897b20de3d7fe9843fe981fe2cb4854
Caio Querino <caioquerino04@gmail.com>
2025-11-07 04:01:52 -0300

    🔄 refactor: implementar interface ContaRequest em UpdateContaRequest

--- conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/dto/requests/conta/UpdateContaRequest.java
+++ conta_bancaria/src/main/java/br/com/conta_bancaria/conta_bancaria/dto/requests/conta/UpdateContaRequest.java
@@ -1,12 +1,13 @@
 package br.com.conta_bancaria.conta_bancaria.dto.requests.conta;
 
 import lombok.*;
+import br.com.conta_bancaria.conta_bancaria.interfaces.ContaRequest;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class UpdateContaRequest {
+public class UpdateContaRequest implements ContaRequest {
     private String tipoConta;
     private double saldo;
     private Long clienteId;
